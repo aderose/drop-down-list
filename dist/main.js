22429ee69e4becb2eb2f6b20fe603b91875391eb
aderose <sandro.derose28@yahoo.com>
2020-10-29 22:20:11 +0000

    Render menu container to the browser

--- dist/main.js
+++ dist/main.js
@@ -7,11 +7,108 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/dropDownMenu.js":
+/*!*****************************!*\
+  !*** ./src/dropDownMenu.js ***!
+  \*****************************/
+/*! namespace exports */
+/*! export default [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_exports__, __webpack_require__.r, __webpack_require__.d, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\n/* harmony import */ var _menuItem__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./menuItem */ \"./src/menuItem.js\");\n\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((function dropDownMenu() {\n  function create(options) {\n    const container = document.createElement('ul');\n    container.setAttribute('class', 'menu-container');\n    options.forEach(({ text, href }) => {\n      container.appendChild(_menuItem__WEBPACK_IMPORTED_MODULE_0__.default.create(text, href).container);\n    });\n    return { container };\n  }\n\n  return { create };\n})());\n\n\n//# sourceURL=webpack://dropdown-list/./src/dropDownMenu.js?");
+
+/***/ }),
+
+/***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/*! unknown exports (runtime-defined) */
-/*! runtime requirements:  */
-eval("console.log('hello world');\n\n\n//# sourceURL=webpack://dropdown-list/./src/index.js?");
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_require__, __webpack_require__.r, __webpack_exports__, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _dropDownMenu__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./dropDownMenu */ \"./src/dropDownMenu.js\");\n\n\nconst myMenu = _dropDownMenu__WEBPACK_IMPORTED_MODULE_0__.default.create([\n  { text: 'Computers', href: '#' },\n  { text: 'Laptops', href: '#' },\n  { text: 'Tablets', href: '#' },\n  { text: 'Mobile Phones', href: '#' },\n  { text: 'Software', href: '#' },\n]);\n\ndocument.body.appendChild(myMenu.container);\n\nconsole.log('hello world');\n\n\n//# sourceURL=webpack://dropdown-list/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/menuItem.js":
+/*!*************************!*\
+  !*** ./src/menuItem.js ***!
+  \*************************/
+/*! namespace exports */
+/*! export default [provided] [no usage info] [missing usage info prevents renaming] */
+/*! other exports [not provided] [no usage info] */
+/*! runtime requirements: __webpack_exports__, __webpack_require__.r, __webpack_require__.d, __webpack_require__.* */
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => __WEBPACK_DEFAULT_EXPORT__\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ((function menuItem() {\n  function create(text, href) {\n    // create the link tag\n    const link = document.createElement('a');\n    link.setAttribute('href', href);\n    link.setAttribute('class', 'menu-link');\n    link.textContent = text;\n\n    // create the item container and append the link\n    const container = document.createElement('li');\n    container.setAttribute('class', 'link-container');\n    container.appendChild(link);\n\n    return {\n      container,\n      link,\n    };\n  }\n\n  return { create };\n})());\n\n\n//# sourceURL=webpack://dropdown-list/./src/menuItem.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => Object.prototype.hasOwnProperty.call(obj, prop)
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import dropDownMenu from './dropDownMenu';
+
+const myMenu = dropDownMenu.create([
+  { text: 'Computers', href: '#' },
+  { text: 'Laptops', href: '#' },
+  { text: 'Tablets', href: '#' },
+  { text: 'Mobile Phones', href: '#' },
+  { text: 'Software', href: '#' },
+]);
+
+document.body.appendChild(myMenu.container);
+
 console.log('hello world');

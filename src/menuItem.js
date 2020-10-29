137c75315df98d643c1d56d41b8ca0613bc5fe69
aderose <sandro.derose28@yahoo.com>
2020-10-29 20:11:36 +0000

    Encapsulated menu item creation within an IIFE

---
+++ src/menuItem.js
@@ -0,0 +1,23 @@
+export default (function menuItem() {
+  function create(text, href) {
+    // create the link tag
+    const link = document.createElement('a');
+    link.setAttribute('href', this.href);
+    link.setAttribute('class', 'menu-link');
+    link.textContent = this.text;
+
+    // create the item container and append the link
+    const container = document.createElement('li');
+    container.setAttribute('class', 'link-container');
+    container.appendChild(link);
+
+    return {
+      text,
+      href,
+      container,
+      link,
+    };
+  }
+
+  return { create };
+})();

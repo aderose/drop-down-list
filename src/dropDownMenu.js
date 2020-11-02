dc48daa555212e04f3b5ac11f070d3ee6e7ba507
aderose <sandro.derose28@yahoo.com>
2020-11-02 16:08:42 +0000

    Implement show and hide functions to the menu factory function

--- src/dropDownMenu.js
+++ src/dropDownMenu.js
@@ -1,13 +1,33 @@
 import menuItem from './menuItem';
 
 export default (function dropDownMenu() {
-  function create(options) {
+  function create({ parent, options }) {
     const container = document.createElement('ul');
     container.setAttribute('class', 'menu-container');
     options.forEach((option) => {
       container.appendChild(menuItem.create(option).container);
     });
-    return { container };
+    parent.appendChild(container);
+
+    function show() {
+      if (container.classList.contains('active')) return;
+      container.classList.add('visible');
+      setTimeout(() => {
+        container.classList.remove('visible');
+        container.classList.add('active');
+      }, 300);
+    }
+
+    function hide() {
+      if (!container.classList.contains('active')) return;
+      container.classList.add('hide');
+      setTimeout(() => {
+        container.classList.remove('hide');
+        container.classList.remove('active');
+      }, 300);
+    }
+
+    return { container, show, hide };
   }
 
   return { create };

0fd3a3409b897dcef9b63a853d3994203cb6860e
aderose <sandro.derose28@yahoo.com>
2020-11-03 15:52:52 +0000

    update click listener to fire on button rather than parent

--- src/menuManager.js
+++ src/menuManager.js
@@ -3,10 +3,13 @@ import dropDownMenu from './dropDownMenu';
 export default (function menuManager() {
   function createMenu({ parentSelector, initialiserSelector, options }) {
     const parent = document.querySelector(parentSelector);
+    const button = document.querySelector(initialiserSelector);
     const menu = dropDownMenu.create({ parent, options });
 
-    parent.addEventListener('click', () => menu.show());
+    // show the menu when the initialiser button is selected
+    button.addEventListener('click', () => menu.show());
 
+    // hide the menu if anything else is selected
     window.addEventListener('click', (e) => {
       if (!e.target.matches(initialiserSelector)) menu.hide();
     });
